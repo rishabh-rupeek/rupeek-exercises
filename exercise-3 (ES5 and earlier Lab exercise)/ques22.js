b03087fc6f2f3bcd1d4e4b7385964d7440b6da65
Rishabh <rishabh.malik@rupeek.com>
2020-10-13 14:42:59 +0530

    ques 22

--- exercise-3 (ES5 and earlier Lab exercise)/ques22.js
+++ exercise-3 (ES5 and earlier Lab exercise)/ques22.js
@@ -2,9 +2,9 @@ const sum = (x,y) => {
     return x+y;
 }
 
-const bindedFunc = sum.bind({x:10})
+const bindedFunc = sum.bind(null,10)
 console.log(bindedFunc(5));
 
-const bindedFunc2 = sum.bind({x:10,y:20})
+const bindedFunc2 = sum.bind(null,10,20)
 //console.log(bindedFunc2);
 console.log(bindedFunc2());
